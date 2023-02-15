2a9705ee273ce5c2f228198321bd4003272028ba
Marc PEREZ <marc.perez.pro@gmail.com>
2023-02-15 12:45:17 +0100

    Cross-platform SSM Parameter Store
    SSM PS doesn't support cross-account, so I have to deploy the resources across all 3 environments. Merge this after https://github.com/hashicorp/terraform/issues/32465

--- aws_ssm_parameter_account_key.tf
+++ aws_ssm_parameter_account_key.tf
@@ -1,6 +1,23 @@
-resource "aws_ssm_parameter" "firebase_service_account_key" {
-  count = length(data.google_projects.firebase_projects.projects)
-  name  = "/google_cloud_ci_cd_service_account_generator/firebase_service_account_keys/${data.google_projects.firebase_projects.projects[count.index].project_id}"
-  type  = "SecureString"
-  value = base64decode(google_service_account_key.firebase_service_account_key[count.index].private_key)
+resource "aws_ssm_parameter" "firebase_service_account_key-dev-eu" {
+  provider = aws.dev-eu
+  count    = length(data.google_projects.firebase_projects.projects)
+  name     = "/google_cloud_ci_cd_service_account_generator/firebase_service_account_keys/${data.google_projects.firebase_projects.projects[count.index].project_id}"
+  type     = "SecureString"
+  value    = base64decode(google_service_account_key.firebase_service_account_key[count.index].private_key)
+}
+
+resource "aws_ssm_parameter" "firebase_service_account_key-qa-eu" {
+  provider = aws.qa-eu
+  count    = length(data.google_projects.firebase_projects.projects)
+  name     = "/google_cloud_ci_cd_service_account_generator/firebase_service_account_keys/${data.google_projects.firebase_projects.projects[count.index].project_id}"
+  type     = "SecureString"
+  value    = base64decode(google_service_account_key.firebase_service_account_key[count.index].private_key)
+}
+
+resource "aws_ssm_parameter" "firebase_service_account_key-prod-eu" {
+  provider = aws.prod-eu
+  count    = length(data.google_projects.firebase_projects.projects)
+  name     = "/google_cloud_ci_cd_service_account_generator/firebase_service_account_keys/${data.google_projects.firebase_projects.projects[count.index].project_id}"
+  type     = "SecureString"
+  value    = base64decode(google_service_account_key.firebase_service_account_key[count.index].private_key)
 }
--- backend.tf
+++ backend.tf
@@ -1,6 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "bfan-terraform-state-bucket"
-    key    = "google_cloud_ci_cd_service_account_generator.tfstate"
+    region  = "eu-west-1"
+    bucket  = "bfan-terraform-state-bucket"
+    key     = "google_cloud_ci_cd_service_account_generator.tfstate"
+    profile = "prod-eu"
   }
 }
